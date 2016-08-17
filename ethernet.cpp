d7654ab3b877a19c894c3bb8c3f95e3a75fceb6a
Daniel Fekete <danieleff@gmail.com>
2016-08-17 06:05:15 +0200

    Show startap messages

--- ethernet.cpp
+++ ethernet.cpp
@@ -2,12 +2,15 @@
 
 #include "ethernet.h"
 #include "settings.h"
+#include "display.h"
 
 #include <EtherCard.h>
 
 #define PIN_ETHERNET_CS 10
 
 LaunchtimeEthernet net;
+
+extern Display display;
 
 // ethernet interface mac address
 static byte mymac[] = { 0x74,0x61,0x62,0x2D,0x30,0x31 };
@@ -16,20 +19,44 @@ byte Ethernet::buffer[600];
 
 extern uint32_t info_downloaded_millis;
 
-void LaunchtimeEthernet::setup() {
-  
-  if (ether.begin(sizeof Ethernet::buffer, mymac, PIN_ETHERNET_CS) == 0) 
-    Serial.println(F("Failed to access Ethernet controller"));
+void loop_error(char* error) {
+  while(1) {
+    display.write(error);
+    delay(800);
+    display.write("        ");
+    delay(200);
+  }
+}
 
-  if (!ether.dhcpSetup())
-    Serial.println(F("DHCP failed"));
+void LaunchtimeEthernet::setup() {
+
+loop_error("DHCP ERR");
+  
+  display.write("NET BOOT");
+  if (ether.begin(sizeof Ethernet::buffer, mymac, PIN_ETHERNET_CS) == 0) {
+      Serial.println(F("Failed to access Ethernet controller"));
+
+      loop_error("NET ERR ");
+  }
+
+
+  display.write("GET DHCP");
+  if (!ether.dhcpSetup()) {
+      Serial.println(F("DHCP failed"));
+
+      loop_error("DHCP ERR");
+  }
     
   ether.printIp(F("My IP: "), ether.myip);
   ether.printIp(F("GW IP: "), ether.gwip);
   ether.printIp(F("DNS IP: "), ether.dnsip);
 
-  if (!ether.dnsLookup(settings.website, true))
-    Serial.println(F("DNS failed"));
+  display.write("GET DNS");
+  if (!ether.dnsLookup(settings.website, true)) {
+      Serial.println(F("DNS failed"));
+      
+      loop_error("DNS ERR ");
+  }
 
   ether.printIp(F("Server: "), ether.hisip);
     
